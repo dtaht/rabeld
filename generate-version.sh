f490e03995a97fb893f635a2b4d4b94bad783b03
Dave Taht <dave.taht@gmail.com>
2017-02-14 10:57:46 -0800

    echo -e is a bash-ism - who knew
    Turns out /bin/sh is not bash on some systems.

--- generate-version.sh
+++ generate-version.sh
@@ -10,4 +10,6 @@ else
     version="unknown"
 fi
 
-echo -e "#ifndef BABELD_VERSION\n#define BABELD_VERSION \"$version\"\n#endif"
+echo "#ifndef BABELD_VERSION"
+echo "#define BABELD_VERSION \"$version\""
+echo "#endif"
