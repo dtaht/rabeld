aef84cce50822aeb9ca173ef86fbc1002ce257cd
Dave Taht <dave.taht@gmail.com>
2017-02-22 06:48:10 -0800

    Killed another pointless memcmp in prefix_cmp
    We are basically testing for inequality and although this version
    *might* be less efficient than memcmp for very short prefixes, even that is
    doubtful.

--- util.h
+++ util.h
@@ -327,28 +327,14 @@ v4tov6(unsigned char *dst, const unsigned char *src)
     memcpy(dst + 12, src, 4);
 }
 
-// This less so as an inline. I'm not really sure as to th
-// purpose of all these compares.
-
 static inline enum prefix_status
 prefix_cmp(const unsigned char *p1, unsigned char plen1,
            const unsigned char *p2, unsigned char plen2)
 {
     int plen = MIN(plen1, plen2);
 
-    if(memcmp(p1, p2, plen / 8) != 0)
+    if(v6_nequal(p1, p2))
         return PST_DISJOINT;
-
-    if(plen % 8 != 0) {
-        int i = plen / 8 + 1;
-	// FIXME - does the shift differ for endianness?
-        unsigned char mask = (0xFF << (plen % 8)) & 0xFF;
-        if((p1[i] & mask) != (p2[i] & mask))
-            return PST_DISJOINT;
-    }
-    // FIXME: I don't see how we ever get here. Ah:
-    // fd99::1:/60
-    // fd99::1:/64
 
     if(plen1 < plen2)
         return PST_LESS_SPECIFIC;
