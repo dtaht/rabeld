091d83a8901f7db68bb7ef929448db98434c5f06
Dave Taht <dave.taht@gmail.com>
2017-02-27 21:12:08 -0800

    Nuke the benchmarks
    All these moved to the libv6 repo elsewhere.

--- benchmarks/benchmark_data.c
+++
@@ -1,9 +0,0 @@
-/**
- * benchmark_data.c
- */
-
-const unsigned char v4prefix[16] =
-    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0xFF, 0xFF, 0, 0, 0, 0 };
-
-const unsigned char llprefix[16] =
-    {0xFE, 0x80};
