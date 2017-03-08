677fbb9077c5f2c751884ea3d39bff7d24287585
Dave Taht <dave.taht@gmail.com>
2017-03-08 06:26:14 -0800

    Some more notes and moving the new logger out of kernel_netlink
    because it's too invasive and I want to be able to A/B the new/old
    behaviors.

--- kernel_netlink.c
+++ kernel_netlink.c
@@ -299,87 +299,6 @@ netlink_socket(struct netlink *nl, uint32_t groups)
     }
 }
 
-#ifdef REPLAY_IPROUTE
-
-static FILE *iproutefd = NULL;
-
-// FIXME: make now a global. We don't need to get or format it every time
-// and we're trying to recreate hairy race conditions here.
-// Hmm. I wonder if we could do multipath with a /whatever on the newgate
-
-int replay_iproute_status(int rc, int errv) {
-	static int err = 0;
-	int errno_safe = errno;
-	if(err != 0) return -1; // Don't try to reopen the file
-	if(iproutefd == NULL) {
-		iproutefd = fopen("/tmp/babel_replay.log","w");
-      		if(iproutefd == NULL) { err++; return -1; }
-	}
-	if(errno == 0) {
-		fprintf(iproutefd, " # OK\n");
-	} else {
-		fprintf(iproutefd, " # rc=%d errno=%d\n", rc, errv);
-	}
-        errno = errno_safe;
-	return rc;
-}
-
-int replay_iproute(int operation, const unsigned char *src, const unsigned char *dest,
- 		   unsigned char src_plen, unsigned char dest_plen,
-		   const unsigned char *gate,	int table, int metric, int ifindex,
- 		   const unsigned char *newgate, int newtable, int newmetric, int newifindex)
-{
-	static struct timeval mynow;
-	static int err = 0;
-	static char buf[64];
-
-	if(err != 0) return -1; // Don't try to reopen the file
-	if(iproutefd == NULL) {
-		iproutefd = fopen("/tmp/babel_replay.log","w");
-      		if(iproutefd == NULL) { err++; return -1; }
-	}
-	if(timeval_compare(&mynow, &now) != 0) {
-		mynow = now;
-		snprintf(buf, sizeof buf, "%ld.%.6ld", mynow.tv_sec, mynow.tv_usec);
-// arguably human readable would be easier on me
-//		strftime(tmbuf, sizeof tmbuf, "%Y-%m-%d %H:%M:%S", mynow.tv_sec);
-//              snprintf(buf, sizeof buf, "%s.%06ld", tmbuf, mynow.tv_usec);
-	}
-	// FIXME: add timestamps
-	char ifname[256];
-	if(v4mapped(dest) && dest_plen > 96) {
-	fprintf(iproutefd, "%s : ip route %s %s "
-            "table %d metric %d dev %s via %s",
-	    buf,
-            operation == ROUTE_ADD ? "add" :
-            operation == ROUTE_FLUSH ? "flush" :
-	    operation == ROUTE_MODIFY ? "replace" : "???",
-            format_prefix(dest, dest_plen),
-		table, metric, if_indextoname(ifindex,ifname), format_address(gate));
-	} else {
-	fprintf(iproutefd, "%s : ip route %s %s from %s "
-            "table %d metric %d dev %s via %s",
-	    buf,
-            operation == ROUTE_ADD ? "add" :
-            operation == ROUTE_FLUSH ? "flush" :
-	    operation == ROUTE_MODIFY ? "replace" : "???",
-            format_prefix(dest, dest_plen), format_prefix(src, src_plen),
-		table, metric, if_indextoname(ifindex,ifname), format_address(gate));
-	}
-	return 0;
-
-}
-
-#else
-int replay_iproute_status(int rc, int errv) { return 0; }
-int replay_iproute(int operation, const unsigned char *src, const unsigned char *dest,
- 		   const unsigned char src_plen, const unsigned char dest_plen,
-		   const unsigned char *gate,	int table, int metric, int ifindex,
-		const unsigned char *newgate, int newtable, int newmetric, int newifindex) {
-   	return 0;
-}
-#endif
-
 static int
 netlink_read(struct netlink *nl, struct netlink *nl_ignore, int answer,
              struct kernel_filter *filter)
@@ -1091,21 +1010,6 @@ kernel_has_ipv6_subtrees(void)
 {
     return (kernel_older_than("Linux", 3, 11) == 0);
 }
-
-/*
-void print_failed_netlink(char *s, int operation, int table, int metric, int dev, int via,
-		char *dest, int plen, char *src, int src_plen, int ifindex, char * gate) {
-
-        fprintf(stderr,"failed kernel_route %s %s from %s "
-            "table %d metric %d dev %s via %s\n",
-            operation == ROUTE_ADD ? "add" :
-            operation == ROUTE_FLUSH ? "flush" :
-	    operation == ROUTE_MODIFY ? "modify" : "???",
-            format_prefix(dest, plen), format_prefix(src, src_plen),
-            table, metric, ifindex(ifindex), format_address(gate));
-	}
-}
-*/
 
 // The way this gets called is confusing
 // "newgate" is only used on a change or replace
